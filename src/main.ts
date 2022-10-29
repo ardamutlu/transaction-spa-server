2e7273c5fdc91d81f73a3242b6a28de384019094
Arda Mutlu <ardamutlu@Ardas-MBP.home>
2022-10-29 23:41:22 +0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(3001);
 }
