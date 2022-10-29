c9bfac29047ac48547ec9e8d329c49c6c576032b
Arda Mutlu <ardamutlu@Ardas-MBP.home>
2022-10-29 23:35:40 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
